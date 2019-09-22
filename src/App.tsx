300b4acdc0b69fe67f48a978e0c427109694efed
AustinBaggio <austinbaggio@gmail.com>
2019-09-22 17:38:31 -0400

    Fake Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,54 @@
-import * as React from 'react';
-import TextField from '@material-ui/core/TextField';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import * as React from "react";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import axios from "axios";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © A&A '}
+      {"Copyright © A&A "}
       {new Date().getFullYear()}
     </Typography>
   );
 }
 
 const useStyles = makeStyles(theme => ({
-  '@global': {
+  "@global": {
     body: {
-      backgroundColor: theme.palette.common.white,
-    },
+      backgroundColor: theme.palette.common.white
+    }
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
-
-const Search = () =>{
- console.log("CLicked")
-}
 
 const App: React.FC = () => {
   const classes = useStyles();
+  const [name, setName] = React.useState(" ");
+  const [data, setData] = React.useState({ names: ["Hello"] });
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        "https://hn.algolia.com/api/v1/search?query=redux"
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
 
   return (
     <Container component="main" maxWidth="md">
@@ -57,13 +65,18 @@ const App: React.FC = () => {
             label="Enter a name"
             name="name"
             autoFocus
+            onChange={e => {
+              setName(e.target.value);
+              console.log(data)
+            }}
           />
         </form>
+        <ul>{name}</ul>
       </div>
       <Box mt={8}>
         <Copyright />
       </Box>
     </Container>
   );
-}
+};
 export default App;
