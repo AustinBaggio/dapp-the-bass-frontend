0aae18c9febd06a2c97d2ea4b957c5b0a7966400
AustinBaggio <austinbaggio@gmail.com>
2019-09-24 22:46:19 -0400

    Added Styling and Bid button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import TextField from "@material-ui/core/TextField";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import {getWhois} from "./api/query"
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import { getWhois } from "./api/query";
 
 function Copyright() {
   return (
@@ -22,17 +26,23 @@ const useStyles = makeStyles(theme => ({
     }
   },
   paper: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(4),
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  form: {
+    width: "100%"
+  },
+  card: {
+    marginTop: theme.spacing(4),
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
   },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
+  row: {
+    display: "flex",
+    flexDirection: "row"
   }
 }));
 
@@ -40,15 +50,26 @@ const App: React.FC = () => {
   const classes = useStyles();
   const [name, setName] = React.useState("");
   React.useEffect(() => {
-    getWhois("austin").then((info) => console.log(info))
+    // getWhois("austin").then((info) => console.log(info))
   }, []);
 
+  const obj = {
+    height: "0",
+    result: {
+      value: "1.1.1.1",
+      owner: "123x123123123123123123123123123123123",
+      price: [
+        {
+          denom: "nametoken",
+          amount: "10"
+        }
+      ]
+    }
+  };
   return (
     <Container component="main" maxWidth="md">
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Lookup a name on node "NODE"
-        </Typography>
+        <Typography variant="h5">Lookup a name on node "NODE"</Typography>
         <form className={classes.form} noValidate>
           <TextField
             variant="outlined"
@@ -64,6 +85,43 @@ const App: React.FC = () => {
           />
         </form>
         <ul>{name}</ul>
+      </div>
+
+      <div className={classes.card}>
+        <Card>
+          <CardHeader
+            title={"IP: " + obj.result.value}
+            subheader={"Owner Addr: " + obj.result.owner}
+            titleTypographyProps={{ align: "left" }}
+            subheaderTypographyProps={{ align: "left" }}
+          />
+          <CardContent>
+            <div
+            style={{marginTop:'-16px'}}
+            >
+              <Typography color="textPrimary">
+                Currency: {obj.result.price[obj.result.price.length - 1].denom}
+                <br />
+                Ammount: {obj.result.price[obj.result.price.length - 1].amount}
+              </Typography>
+            </div>
+          </CardContent>
+          <form noValidate>
+            <div className={classes.row}>
+              <TextField
+                variant="outlined"
+                id="bid"
+                fullWidth
+                label="Enter a higher bid"
+                name="bid"
+                autoFocus
+              />
+              <Button fullWidth variant="contained">
+                Submit Bid
+              </Button>
+            </div>
+          </form>
+        </Card>
       </div>
       <Box mt={8}>
         <Copyright />
